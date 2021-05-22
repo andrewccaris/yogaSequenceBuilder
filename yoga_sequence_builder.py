7cd357a9d2346baf78b17ee8b98962a717f9a5d4
andrewccaris <cookiemonster.guru@gmail.com>
2021-05-22 12:49:34 -0700

    start building 60 min sequence

--- yoga_sequence_builder.py
+++ yoga_sequence_builder.py
@@ -1,7 +1,8 @@
 import yaml
 import os
+import random
 
-def print_dictionary( asanas ):
+def print_asana_dictionary( asanas ):
     for type,poses in asanas.items():
         for pose in poses:
             for key,name in pose.items():
@@ -13,17 +14,50 @@ def print_dictionary( asanas ):
                 print('Sanskrit: ' + name['sanskrit'])
                 print('Orientation: ' + name['orientation'])
 
-def get_asana_rotation( asanas, type, key ):
-    return asanas[type][key][key]['rotation']
+def print_asana_attributes( asana ):
+    print(get_asana_english_name(asana))
+    print(get_asana_sanskrit_name(asana))
+    print(get_asana_rotation(asana))
+    print(get_asana_orientation(asana))
 
-def get_asana_english_name( asanas, type, key ):
-    return asanas[type][key][key]['english']
+def get_asana_rotation( asana ):
+    return asana['rotation']
 
-def get_asana_sanskrit_name( asanas, type, key ):
-    return asanas[type][key][key]['sanskrit']
+def get_asana_english_name( asana ):
+    return asana['english']
 
-def get_asana_orientation( asanas, type, key ):
-    return asanas[type][key][key]['orientation']
+def get_asana_sanskrit_name( asana ):
+    return asana['sanskrit']
+
+def get_asana_orientation( asana ):
+    return asana['orientation']
+
+def get_random_asana( full_asanas, type ):
+    asanas = full_asanas[type]
+    key = random.randrange(0, len(asanas), 1)
+    return asanas[key][key]
+
+#TODO: Add link move between standing and floor and floor and seated, etc
+#TODO: Don't allow duplicates
+#TODO: Find out how many moves are in a 60 min sequence, based on length of each move for 5 breaths and 1 breath for some
+def build_60min_sequence( full_asanas ):
+    link = 0
+    for x in range(0,40):
+        if x < 25:
+            if link == 3:
+                asana = get_random_asana(full_asanas, 'linkers')
+                print(get_asana_english_name(asana))
+                link = 0
+            else:
+                asana = get_random_asana(full_asanas, 'rising-action')
+                print(get_asana_english_name(asana))
+                link += 1
+        elif x > 25 and x < 28:
+            asana = get_random_asana( full_asanas, 'climax')
+            print( get_asana_english_name(asana))
+        else:
+            asana = get_random_asana(full_asanas, 'falling-action')
+            print(get_asana_english_name(asana))
 
 if __name__ == '__main__':
     __location__ = os.path.realpath( 
@@ -35,10 +69,8 @@ if __name__ == '__main__':
     # climax = {key:value for key,value in full_asanas.items() if key in 'climax'}
     # falling = {key:value for key,value in full_asanas.items() if key in 'falling-action'}
     
-    print(get_asana_english_name(full_asanas,'linkers',1))
-    print(get_asana_english_name(full_asanas,'rising-action',10))
-    print(get_asana_english_name(full_asanas,'climax',2))
-    print(get_asana_english_name(full_asanas,'falling-action',12))
+    build_60min_sequence(full_asanas)
+
     # print_dictionary(full_asanas['linkers'])
     # print_dictionary(full_asanas['rising-action'])
     # print_dictionary(full_asanas['climax'])
