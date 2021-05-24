3c6db1bc9bcdbbd3b9c804f4e1124ab52b861762
andrewccaris <cookiemonster.guru@gmail.com>
2021-05-24 15:58:54 -0700

    classify yoga sequence builder
    Fix up formatting
    Add todo to regenerate sequences

--- app.py
+++ app.py
@@ -3,22 +3,24 @@ from kivy.lang import Builder
 from kivymd.app import MDApp
 from kivymd.uix.list import TwoLineListItem
 
-import yoga_sequence_builder
+from yoga_sequence_builder import YogaSequenceBuilder
 
+#TODO: Add button to regenerate sequences
 class Main(MDApp):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.screen = Builder.load_file('layout.kv')
 
     def build(self):
-        for asana in yoga_sequence_builder.get_sequence(60):
+        yoga = YogaSequenceBuilder()
+        for asana in yoga.get_sequence(60):
             self.screen.ids.box_sixty_minute_sequence.add_widget(
                 TwoLineListItem(
                     text= f"{asana[0]}",
                     secondary_text= f"{asana[1]}"
                 )
             )
-        for asana in yoga_sequence_builder.get_sequence(90):
+        for asana in yoga.get_sequence(90):
             self.screen.ids.box_ninety_minute_sequence.add_widget(
                 TwoLineListItem(
                     text= f"{asana[0]}",
--- yoga_sequence_builder.py
+++ yoga_sequence_builder.py
@@ -2,86 +2,88 @@ import yaml
 import os
 import random
 
-def print_asana_dictionary( asanas ):
-    for type,poses in asanas.items():
-        for pose in poses:
-            for key,name in pose.items():
-                print()
-                print(type)
-                print('Key: ' + str(key))
-                print('Rotation: ' + name['rotation'])
-                print('English: ' + name['english'])
-                print('Sanskrit: ' + name['sanskrit'])
-                print('Orientation: ' + name['orientation'])
+class YogaSequenceBuilder:
+        
+    def print_asana_dictionary(self, asanas):
+        for type,poses in asanas.items():
+            for pose in poses:
+                for key,name in pose.items():
+                    print()
+                    print(type)
+                    print('Key: ' + str(key))
+                    print('Rotation: ' + name['rotation'])
+                    print('English: ' + name['english'])
+                    print('Sanskrit: ' + name['sanskrit'])
+                    print('Orientation: ' + name['orientation'])
 
-def print_asana_attributes( asana ):
-    print(get_asana_english_name(asana))
-    print(get_asana_sanskrit_name(asana))
-    print(get_asana_rotation(asana))
-    print(get_asana_orientation(asana))
+    def print_asana_attributes(self, asana):
+        print(self.get_asana_english_name(asana))
+        print(self.get_asana_sanskrit_name(asana))
+        print(self.get_asana_rotation(asana))
+        print(self.get_asana_orientation(asana))
 
-def get_asana_rotation( asana ):
-    return asana['rotation']
+    def get_asana_rotation(self, asana):
+        return asana['rotation']
 
-def get_asana_english_name( asana ):
-    return asana['english']
+    def get_asana_english_name(self, asana):
+        return asana['english']
 
-def get_asana_sanskrit_name( asana ):
-    return asana['sanskrit']
+    def get_asana_sanskrit_name(self, asana):
+        return asana['sanskrit']
 
-def get_asana_orientation( asana ):
-    return asana['orientation']
+    def get_asana_orientation(self, asana):
+        return asana['orientation']
 
-def get_specific_asana( full_asanas, type, key ):
-    return full_asanas[type][key][key]
+    def get_specific_asana(self, full_asanas, type, key):
+        return full_asanas[type][key][key]
 
-def get_random_asana( full_asanas, type ):
-    asanas = full_asanas[type]
-    key = random.randrange(0, len(asanas), 1)
-    return asanas[key][key]
+    def get_random_asana(self, full_asanas, type):
+        asanas = full_asanas[type]
+        key = random.randrange(0, len(asanas), 1)
+        return asanas[key][key]
 
-#TODO: Add link move between standing and floor and floor and seated, etc
-#TODO: Add rotation cannot go from internal to external, vice versa, either external to external or to neutral
-#TODO: Find out how many moves are in a 60 min sequence, based on length of each move for 5 breaths and 1 breath for some
-#TODO: Add warm up sequence
-def build_sequence( full_asanas, vinyasa_flow, time ):
-    list = []
-    link = False
-    moves = int( time / 1.5 )
-    rising_moves = int( moves / 1.6 )
-    asana_name = ""
-    for x in range(0, moves):
-        if x < rising_moves:
-            if x % 3 == 0 and x != 0:
-                if vinyasa_flow:
-                    asana = get_specific_asana(full_asanas, 'linkers', 4)
+    #TODO: Add link move between standing and floor and floor and seated, etc
+    #TODO: Add rotation cannot go from internal to external, vice versa, either external to external or to neutral
+    #TODO: Find out how many moves are in a 60 min sequence, based on length of each move for 5 breaths and 1 breath for some
+    #TODO: Add warm up sequence
+    def build_sequence(self, full_asanas, vinyasa_flow, time):
+        list = []
+        link = False
+        moves = int(time / 1.5)
+        rising_moves = int(moves / 1.6)
+        asana_name = ""
+        for x in range(0, moves):
+            if x < rising_moves:
+                if x % 3 == 0 and x != 0:
+                    if vinyasa_flow:
+                        asana = self.get_specific_asana(full_asanas, 'linkers', 4)
+                    else:
+                        asana = self.get_random_asana(full_asanas, 'linkers')
+                    link = True
                 else:
-                    asana = get_random_asana(full_asanas, 'linkers')
-                link = True
+                    asana = self.get_random_asana(full_asanas, 'rising-action')
+            elif x > rising_moves and x < rising_moves+3:
+                asana = self.get_random_asana(full_asanas, 'climax')
             else:
-                asana = get_random_asana(full_asanas, 'rising-action')
-        elif x > rising_moves and x < rising_moves+3:
-            asana = get_random_asana( full_asanas, 'climax')
-        else:
-            asana = get_random_asana(full_asanas, 'falling-action')
+                asana = self.get_random_asana(full_asanas, 'falling-action')
 
-        asana_names = (get_asana_sanskrit_name(asana), get_asana_english_name(asana) )
-        if link or asana_names not in list:
-            list.append( asana_names )
-            link = False
-        else:
-            x = x - 1
-            
-            
-    return list
+            asana_names = (self.get_asana_sanskrit_name(asana), self.get_asana_english_name(asana))
+            if link or asana_names not in list:
+                list.append(asana_names)
+                link = False
+            else:
+                x = x - 1
+                
+                
+        return list
 
-def get_sequence_as_string( sequence ):
-    return '\n'.join(sequence)
+    def get_sequence_as_string(self, sequence):
+        return '\n'.join(sequence)
 
-def get_sequence( time ):
-    __location__ = os.path.realpath( 
-        os.path.join(os.getcwd(), os.path.dirname(__file__)))
-    stream = open(os.path.join(__location__,'asanas.yml'), 'r')
-    full_asanas = yaml.safe_load(stream)
+    def get_sequence(self, time):
+        __location__ = os.path.realpath(
+            os.path.join(os.getcwd(), os.path.dirname(__file__)))
+        stream = open(os.path.join(__location__,'asanas.yml'), 'r')
+        full_asanas = yaml.safe_load(stream)
 
-    return build_sequence(full_asanas, True, int(time))
+        return self.build_sequence(full_asanas, True, int(time))
